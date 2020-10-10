4574f8b9888f866aff1aae828510eb1072f2a44f
Carl <carl.hostrander00@gmail.com>
2020-10-09 22:49:40 -0500

    changing to library

--- main.go
+++ main.go
@@ -1,32 +1,7 @@
-package main
+package helloworld
 
-import (
-	"fmt"
-	"github.com/gorilla/mux"
-	"io"
-	"log"
-	"net/http"
-	"time"
-)
+import "fmt"
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello there!\n")
-}
-
-func router() *mux.Router {
-	r := mux.NewRouter()
-	r.HandleFunc("/", handler)
-	return r
-}
-func main() {
-	router := router()
-	fmt.Println("Hello, world!")
-	srv := &http.Server{
-		Handler:      router,
-		Addr:         "127.0.0.1:9100",
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout:  15 * time.Second,
-	}
-
-	log.Fatal(srv.ListenAndServe())
+func Greeting() {
+	fmt.Println("Hello Golang world")
 }
